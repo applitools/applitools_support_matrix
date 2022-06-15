8974d6189d49ae43b98e070425b5b91f71afb2d7
artem <artem.borodavka@applitools.com>
2022-06-15 16:07:01 +0300

    Refresh the repo add ruby tests add structure sdks matrix

---
+++ sdks/ruby/spec/selenium_helper.rb
@@ -0,0 +1,58 @@
+require 'eyes_selenium'
+
+RSpec.shared_context "Common" do
+  let(:options) {
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+  }
+  let(:driver) {
+    if ENV.key?('CI')
+      driver = Selenium::WebDriver.for :chrome, options: options
+    else
+      caps = Selenium::WebDriver::Remote::Capabilities.chrome
+      caps['goog:chromeOptions'] = {
+        args: ['headless']
+      }
+      driver = Selenium::WebDriver.for :remote, capabilities: caps, url: 'http://localhost:4444/wd/hub'
+    end
+    driver
+  }
+  let(:ufg) { false }
+
+  let(:runner) {
+    if ufg
+      Applitools::Selenium::VisualGridRunner.new(10)
+    else
+      Applitools::ClassicRunner.new
+    end
+  }
+
+  let(:eyes) {
+    eyes = Applitools::Selenium::Eyes.new(runner: runner)
+    eyes.configure do |conf|
+      # conf.batch = $run_batch
+      conf.api_key = ENV['APPLITOOLS_API_KEY']
+      conf.branch_name = 'master'
+      conf.parent_branch_name = 'master'
+      conf.save_new_tests = false
+      conf.hide_caret = true
+    end
+    eyes.match_timeout = 0 unless ufg
+    eyes
+  }
+
+end
+
+RSpec.configure do |config|
+
+  config.include_context "Common"
+  config.before(:each) do
+    driver
+  end
+  config.after(:each) do
+    driver.quit
+    eyes.abort
+    runner.get_all_test_results(false)
+  end
+
+end
