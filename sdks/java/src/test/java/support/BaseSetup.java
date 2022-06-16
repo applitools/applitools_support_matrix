17b0adc5801e6f8448b0b71fa82ba0aad4bd6056
artem <artem.borodavka@applitools.com>
2022-06-16 20:01:45 +0300

    lower testng version update update script to update only applitools packages

--- sdks/java/src/test/java/support/BaseSetup.java
+++ sdks/java/src/test/java/support/BaseSetup.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 import java.net.MalformedURLException;
 import java.net.URL;
