72a9f73443807e759f13ae0a290232dd110f618a
artem <artem.borodavka@applitools.com>
2022-06-27 19:18:33 +0300

    update hostOS

--- sdks/js/eyes-playwright/index.ts
+++ sdks/js/eyes-playwright/index.ts
@@ -18,6 +18,23 @@ export function setupEyes({vg, ...config}) {
         saveNewTests: false,
         ...config,
     }
+    if(process.env.RUNNER_OS) {
+        switch (process.env.RUNNER_OS) {
+            case 'macOS':
+                // @ts-ignore
+                configuration.hostOS = 'Mac OS X 10.15'
+                break;
+            case 'Windows':
+                // @ts-ignore
+                configuration.hostOS = 'Windows 10'
+                break;
+            case 'Linux':
+                // @ts-ignore
+                configuration.hostOS = 'Linux'
+                break;
+        }
+
+    }
     const eyes = new Eyes(runner)
     eyes.setConfiguration(configuration)
     return eyes
