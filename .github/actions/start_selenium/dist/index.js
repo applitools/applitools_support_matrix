b61f5994f044446ea8b25067fd8c8957e8aeac7d
artem <artem.borodavka@applitools.com>
2022-08-01 23:06:19 +0300

    update action

--- .github/actions/start_selenium/dist/index.js
+++ .github/actions/start_selenium/dist/index.js
@@ -9008,13 +9008,10 @@ const github = __nccwpck_require__(2566);
 try {
     // `who-to-greet` input defined in action metadata file
     const legacy = core.getInput('legacy');
-    console.log(github.context)
-    console.log(`Selenium version is set to ${legacy}!`);
+    console.log(process.env)
+    console.log(`Selenium version is set to ${legacy ? "3" : "4"}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
