bd4099abfc61e662ceac1c9832aa74fe40604368
artem <artem.borodavka@applitools.com>
2022-08-02 17:59:48 +0300

    add protractor setup

---
+++ sdks/js/eyes-protractor/index.js
@@ -0,0 +1,42 @@
+const {Eyes, VisualGridRunner, Target} = require("@applitools/eyes-protractor")
+
+async function setupDriver() {
+    return browser;
+}
+
+async function teardownDriver(driver) {
+    return
+}
+
+async function open(driver, url) {
+    await browser.get(url)
+}
+
+const batch = {
+    name: process.env.APPLITOOLS_BATCH_NAME || 'JS Support Matrix Eyes Protractor',
+}
+
+function setupEyes({vg, ...config}) {
+    const runner = (vg ? new VisualGridRunner({testConcurrency: 500}) : undefined)
+    const configuration = {
+        apiKey: process.env.APPLITOOLS_API_KEY,
+        batch,
+        parentBranchName: 'master',
+        branchName: 'master',
+        dontCloseBatches: true,
+        matchTimeout: 0,
+        saveNewTests: false,
+        ...config,
+    }
+    const eyes = new Eyes(runner)
+    eyes.setConfiguration(configuration)
+    return eyes
+}
+
+module.exports = {
+    setupEyes,
+    setupDriver,
+    teardownDriver,
+    open,
+    Target,
+}
