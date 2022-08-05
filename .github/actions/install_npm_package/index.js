1a126fe1622d56ea4cff7d17d9d763d776cf8ea4
artem <artem.borodavka@applitools.com>
2022-08-05 15:43:57 +0300

    check getting versions

--- .github/actions/install_npm_package/index.js
+++ .github/actions/install_npm_package/index.js
@@ -1,23 +1,30 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import * as fetch from 'node-fetch'
-import {execSync} from 'child_process'
 import {get} from 'https'
 import * as fs from 'fs'
 import path from 'path'
+import {Version, parseVersion, strToNum, shellCommand, getLatest, getAllVersions} from "./util";
 
 try {
     const packageName = core.getInput('package');
-    const major = core.getInput('major');
-    const minor = core.getInput('minor');
-    const patch = core.getInput('patch');
+    const major = strToNum(core.getInput('major'));
+    const minor = strToNum(core.getInput('minor'));
+    const patch = strToNum(core.getInput('patch'));
     const dir = core.getInput('working-directory');
+    const cwd = path.join(process.cwd(), dir)
     console.log(`Package name: ${packageName} | type: ${typeof packageName}`)
     console.log(`Major: ${major} | type: ${typeof major}`)
     console.log(`Minor: ${minor} | type: ${typeof minor}`)
     console.log(`Patch: ${patch} | type: ${typeof patch}`)
     console.log(`Dir: ${dir} | type: ${typeof dir}`)
-    console.log(path.join(process.cwd(), dir))
+    console.log(cwd)
+    const latest = getLatest(packageName, cwd)
+    console.log(latest)
+    const all = getAllVersions(packageName, cwd)
+    console.log(all[0])
+    console.log(all[1])
+    console.log(all[2])
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
 } catch (error) {
---
+++ .github/actions/install_npm_package/util.js
@@ -0,0 +1,69 @@
+'use strict'
+import {execSync} from 'child_process'
+class Version {
+    constructor({major, minor, patch}) {
+        this.major = strToNum(major);
+        this.minor = strToNum(minor);
+        this.patch = strToNum(patch);
+    }
+
+    compare(another) {
+        let result = compareNums(this.major, another.major)
+        if (result === 0) result = compareNums(this.minor, another.minor)
+        if (result === 0) result = compareNums(this.patch, another.patch)
+        return result
+
+        function compareNums(a, b) {
+            if (a > b) return -1
+            else if (a < b) return 1
+            else return 0
+        }
+    }
+
+}
+
+function parseVersion(versionString) {
+    const reg_version_parse = /(\d+).(\d+).(\d+)/gm
+    const arr = reg_version_parse.exec(versionString);
+    if (arr === null) {
+        console.log(versionString)
+        throw new Error("failed to parse")
+    }
+    return new Version({
+        major: arr[1],
+        minor: arr[2],
+        patch: arr[3],
+    })
+}
+
+
+function strToNum(str) {
+    const parsed = parseInt(str)
+    if (isNaN(parsed)) {
+        throw new Error(`Tried to parse string [${str}] to the num`)
+    }
+    return parsed
+}
+
+function shellCommand(command, cwd) {
+    return  execSync(command, {cwd}).toString();
+}
+
+function getLatest(packageName, cwd) {
+    return parseVersion(shellCommand(`npm show ${packageName} version`, cwd))
+}
+
+function getAllVersions(packageName, cwd) {
+    const commandRes = shellCommand(`npm show ${packageName} versions`, cwd);
+    const reg_versions = /'\d+.\d+.\d+'/gm;
+    return commandRes.match(reg_versions).map(parseVersion).sort((a,b) => a.compare(b));
+}
+
+export {
+    Version,
+    parseVersion,
+    strToNum,
+    shellCommand,
+    getLatest,
+    getAllVersions,
+}
