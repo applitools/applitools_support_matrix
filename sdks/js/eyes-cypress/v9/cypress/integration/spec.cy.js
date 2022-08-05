81c4e2e1a08af74c29bf6b4931eaed03bd3c29da
artem <artem.borodavka@applitools.com>
2022-08-05 20:39:06 +0300

    update v9 spec to be compatible with older versions of cypress update job names

--- sdks/js/eyes-cypress/v9/cypress/integration/spec.cy.js
+++ sdks/js/eyes-cypress/v9/cypress/integration/spec.cy.js
@@ -2,16 +2,19 @@ describe('UFG', () => {
 
   beforeEach(() => {
     cy.visit('https://applitools.github.io/demo/TestPages/FramesTestPage/')
-    cy.eyesOpen({
-      testName: Cypress.currentTest.title,
-    })
   })
 
   it('Window - UFG', () => {
+    cy.eyesOpen({
+      testName: 'Window - UFG',
+    })
     cy.eyesCheckWindow({target:'window', fully: true})
   })
 
   it('Region - UFG', () => {
+    cy.eyesOpen({
+      testName: 'Region - UFG',
+    })
     cy.eyesCheckWindow({target: 'region',region: {x: 50, y: 70, width: 90, height: 110}})
   })
 
