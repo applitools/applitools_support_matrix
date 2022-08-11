a09406ab6e97f8fe6eb7dd53eb2fcc023526b338
Artem-applitools <59561599+Artem-applitools@users.noreply.github.com>
2022-08-11 11:55:28 +0300

    Wip (#4)
    * update js framework sdks to run on not only on latest version of the framework
    * update job names for the matrix
    * change nightwatch to the latest 1 major version
    * change storybook workflow to have branch env var

--- sdks/js/eyes-storybook/applitools.config.js
+++ sdks/js/eyes-storybook/applitools.config.js
@@ -3,7 +3,7 @@ module.exports = {
   apiKey: process.env.APPLITOOLS_API_KEY,
   appName: 'Applitools Support Matrix',
   batchName: process.env.APPLITOOLS_BATCH_NAME || 'JS Support Matrix Eyes Storybook',
-  branchName: 'master',
+  branchName: process.env.APPLITOOLS_BRANCH_NAME || 'master',
   parentBranchName: 'master',
   browser: [
     {width: 700, height: 460, name: 'chrome'},
