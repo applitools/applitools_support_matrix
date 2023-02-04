5e93c31a79604a139cbb5576a09183ab0ada403b
artem <artem.borodavka@applitools.com>
2023-02-04 03:34:38 +0200

    add setup action update workflow to use setup action

---
+++ .github/actions/setup/index.js
@@ -0,0 +1,7 @@
+import * as core from '@actions/core'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const work_dir = core.getInput('work_dir');
+const matrix = fs.readFileSync(path.join(process.cwd(), work_dir, 'matrix.json')).toString();
+core.setOutput("matrix", JSON.stringify(matrix));
