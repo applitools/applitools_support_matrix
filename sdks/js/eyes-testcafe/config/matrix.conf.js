50c962a7473809cea87f70465614508f633db86c
artem <artem.borodavka@applitools.com>
2023-02-10 00:14:55 +0200

    update testcafe workflow

---
+++ sdks/js/eyes-testcafe/config/matrix.conf.js
@@ -0,0 +1,25 @@
+const common = {
+    "node-version": "18",
+    "work_dir": "sdks/js/eyes-testcafe",
+    "framework_package": "testcafe",
+    test_command: "npm test"
+}
+const base_variations = [
+    {
+        "os": "ubuntu-latest",
+        "version": "latest@",
+    },
+    {
+        "os": "ubuntu-latest",
+        "version": "major@1",
+    },
+    {
+        "os": "windows-latest",
+        "version": "latest@",
+    }
+]
+const variations = base_variations.map(variant => ({...common, ...variant, job_name:`JS Testcafe [${variant.os} | ${variant.version}]`}))
+console.log(variations)
+module.exports = {
+    "include": variations
+}
