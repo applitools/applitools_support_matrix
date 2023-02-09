1428756c51b5b32b01d680ff328bf75ba16b3811
artem <artem.borodavka@applitools.com>
2023-02-09 19:23:31 +0200

    update setup action

--- .github/actions/setup/dist/index.js
+++ .github/actions/setup/dist/index.js
@@ -2827,7 +2827,7 @@ const fileName = use_last_passed ? `${config_file}_passed.conf.js` : `${config_f
 // Adding hardcoded string is required for ncc to build it properly into 1 file
 const filePath = path.join(process.cwd(), work_dir, 'config',  fileName)
 const readedFile = require(filePath)
-const matrix = JSON.parse(readedFile)
+const matrix = readedFile
 console.log(JSON.stringify(matrix, null, 3))
 setOutput("matrix", JSON.stringify(matrix));
 })();
