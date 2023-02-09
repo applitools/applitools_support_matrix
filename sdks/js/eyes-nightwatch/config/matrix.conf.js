c4b46529a916f4d4ca543f0a454c64cbd28b08e8
artem <artem.borodavka@applitools.com>
2023-02-09 21:53:45 +0200

    update nightwatch and playwright

---
+++ sdks/js/eyes-nightwatch/config/matrix.conf.js
@@ -0,0 +1,26 @@
+const common = {
+    "node-version": "18",
+    "work_dir": "sdks/js/eyes-nightwatch",
+    "framework_package": "nightwatch",
+    use_selenium: true,
+    test_command: "npm test && USE_UFG=true npm test"
+}
+const base_variations = [
+    {
+        "os": "ubuntu-latest",
+        "version": "exact@1.7.13",
+    },
+    {
+        "os": "windows-latest",
+        "version": "exact@1.7.13",
+    },
+    {
+        "os": "macos-latest",
+        "version": "exact@1.7.13",
+    }
+]
+const variations = base_variations.map(variant => ({...common, ...variant, job_name:`JS Nightwatch [${variant.os}]`}))
+console.log(variations)
+module.exports = {
+    "include": variations
+}
