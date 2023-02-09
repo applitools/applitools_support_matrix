4035e42956a22d5cf214649abbbe2bd778cbbf49
artem <artem.borodavka@applitools.com>
2023-02-09 20:27:08 +0200

    update cypress and js selenium flows

---
+++ sdks/js/eyes-cypress/config/matrix.conf.js
@@ -0,0 +1,53 @@
+const common = {
+    "node-version": "18",
+    "work_dir": "sdks/js/eyes-cypress/v10",
+    "framework_package": "cypress",
+    test_command: "npm test"
+}
+const latest_variations = [
+    {
+        "os": "ubuntu-latest",
+        "version": "latest@",
+    },
+    {
+        "os": "ubuntu-latest",
+        "version": "major@1",
+    },
+    {
+        "os": "ubuntu-latest",
+        "version": "major@2",
+    },
+    {
+        "os": "windows-latest",
+        "version": "latest@",
+    },
+    {
+        "os": "macos-latest",
+        "version": "latest@",
+    }
+]
+
+const old_variations = [
+    {
+        "os": "ubuntu-latest",
+        "version": "exact@9.7.0",
+    },
+    {
+        "os": "ubuntu-latest",
+        "version": "exact@8.7.0",
+    },
+]
+
+
+const base_common = latest_variations.map(variant => ({...common, ...variant,})).concat(old_variations.map(variant => ({
+    ...common, ...variant,
+    work_dir: "sdks/js/eyes-cypress/v9"
+})))
+const variations = base_common.map(variant => ({
+    ...variant,
+    job_name: `JS Cypress [${variant.os} | ${variant.version}]`
+}))
+console.log(variations)
+module.exports = {
+    "include": variations
+}
--- sdks/js/eyes-selenium/config/matrix.conf.js
+++ sdks/js/eyes-selenium/config/matrix.conf.js
@@ -21,6 +21,7 @@ const base_variations = [
 const base_common = base_variations.map(variant => ({...common, ...variant,}))
 const variations = base_common.map((variant) => ({...variant, use_selenium: true, test_command: "npm test"}))
     .concat(base_common.map(variant => ({...variant, test_command: "npm run appium"})))
+    .map(variant => ({...variant, job_name:`JS ${variant.use_selenium ? 'Selenium' : 'Appium'} [${variant.os}]`}))
 console.log(variations)
 module.exports = {
     "include": variations
