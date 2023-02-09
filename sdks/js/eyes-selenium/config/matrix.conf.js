b192645275f461d8dd43a484b4cb82e6dd3232ca
artem <artem.borodavka@applitools.com>
2023-02-09 19:50:23 +0200

    update test run action update selenium package setup

--- sdks/js/eyes-selenium/config/matrix.conf.js
+++ sdks/js/eyes-selenium/config/matrix.conf.js
@@ -1,25 +1,27 @@
-module.exports = {
-  "include": [
+const common = {
+    "node-version": "18",
+    "work_dir": "sdks/js/eyes-selenium",
+    "js_config": "eyes-selenium",
+    "framework_package": "selenium-webdriver"
+}
+const base_variations = [
     {
-      "os": "ubuntu-latest",
-      "node-version": "16",
-      "version": "latest@",
-      "work_dir": "sdks/js/eyes-selenium",
-      "js_config": "eyes-selenium"
+        "os": "ubuntu-latest",
+        "version": "latest@",
     },
     {
-      "os": "windows-latest",
-      "node-version": "16",
-      "version": "latest@",
-      "work_dir": "sdks/js/eyes-selenium",
-      "js_config": "eyes-selenium"
+        "os": "windows-latest",
+        "version": "latest@",
     },
     {
-      "os": "macos-latest",
-      "node-version": "16",
-      "version": "latest@",
-      "work_dir": "sdks/js/eyes-selenium",
-      "js_config": "eyes-selenium"
+        "os": "macos-latest",
+        "version": "latest@",
     }
-  ]
+]
+const base_common = base_variations.map(variant => ({...common, ...variant,}))
+const variations = base_common.map((variant) => ({...variant, use_selenium: true, test_command: "npm test"}))
+    .concat(base_common.map(variant => ({...variant, test_command: "npm run appium"})))
+console.log(variations)
+module.exports = {
+    "include": variations
 }
