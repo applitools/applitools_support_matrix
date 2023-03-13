fe32c36f0a34176cb0d8494dacf130ae9ba4fbe0
artem <artem.borodavka@applitools.com>
2023-03-14 00:44:13 +0200

    fix action

--- .github/actions/setup/dist/index.js
+++ .github/actions/setup/dist/index.js
@@ -2833,26 +2833,26 @@ if (use_last_passed) {
     const filePath = path.join(process.cwd(), 'last_passed.json');
     const json_string = fs.readFileSync(filePath).toString();
     const last_passed = JSON.parse(json_string)
-    const include = [];
+    let include = [];
     dirs.forEach(dir => {
         const matrix_jobs = last_passed.data.filter(suite => suite.config_path === dir)[0]
         const dir_include = matrix_jobs.jobs.map(job => ({
             ...JSON.parse(job.matrix_string),
             version: `exact@${job.version}`
         }))
-        include.concat(dir_include)
+        include = include.concat(dir_include)
     })
 
     matrix = {include}
 } else {
     // Adding hardcoded string is required for ncc to build it properly into 1 file
     const fileName =  `matrix.conf.js`;
-    const include = [];
+    let include = [];
     dirs.forEach(dir => {
         const filePath = path.join(process.cwd(), dir, 'config',  fileName)
         console.log(`Filepath value: ${filePath}`)
         const readedFile = require(filePath)
-        include.concat(readedFile.include.map(matrix_data => ({...matrix_data, matrix_config_dir:dir})))
+        include = include.concat(readedFile.include.map(matrix_data => ({...matrix_data, matrix_config_dir:dir})))
     })
 
     matrix = {
