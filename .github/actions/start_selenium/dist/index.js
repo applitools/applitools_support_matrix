b53a4b8d9c31248fd9fd40b4b03f869269e37b07
artem <artem.borodavka@applitools.com>
2023-03-29 12:23:11 +0300

    update logs

--- .github/actions/start_selenium/dist/index.js
+++ .github/actions/start_selenium/dist/index.js
@@ -6673,6 +6673,7 @@ class SeleniumParser extends CoreParser {
         const raw_data = await fetch(SELENIUM_LATEST_RELEASES_INFO_URL).then(res => {
             console.log(res.ok)
             console.log(res.status)
+            console.log(res.statusText)
             if (!res.ok) console.log(JSON.stringify(res, null, 3))
             return  res.text()
         })
