d408a202ca8db387c610edc7db5c81230f3903f6
artem <artem.borodavka@applitools.com>
2023-04-04 17:23:46 +0300

    update story book to have separate setups for 6+ and 7+ versions

---
+++ sdks/js/eyes-storybook/latest/.storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.jsx'],
+  // stories: [],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
