99918ca5f2923c9ee585eb8377a1e77e1667d707
artem <artem.borodavka@applitools.com>
2023-04-14 11:31:11 +0300

    fix puppeteer

--- sdks/js/.mocharc.js
+++ sdks/js/.mocharc.js
@@ -2,7 +2,6 @@ module.exports = {
     spec: [
         '../tests/support/*.spec.js',
     ],
-    reporter: 'mocha-allure-reporter',
     parallel: true,
     jobs: process.env.MOCHA_JOBS || 2,
     timeout: 0,
