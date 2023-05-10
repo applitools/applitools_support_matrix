20b6f25c56df7b4dd700557cf9f9abf111c6d8dc
Artem Borodavka <artem.borodavka@applitools.com>
2023-05-10 11:35:02 +0300

    add java to the alpine tests

--- sdks/java/config/matrix.conf.js
+++ sdks/java/config/matrix.conf.js
@@ -17,54 +17,87 @@ const base_variations = [
         version: "latest@"
     }
 ]
+const alpine = {
+    use_container: true,
+    container: 'artem0tranduil/alpine_runner:latest',
+    service: 'selenium/standalone-chrome:latest',
+}
+const selenium_package = {
+    work_dir: "sdks/java/selenium",
+    framework_package: {
+        groupId: 'org.seleniumhq.selenium',
+        artifactId: 'selenium-java'
+    },
+    eyes_package: {
+        groupId: 'com.applitools',
+        artifactId: 'eyes-selenium-java5'
+    },
+}
+const playwright_package = {
+    work_dir: "sdks/java/playwright",
+    framework_package: {
+        groupId: 'com.microsoft.playwright',
+        artifactId: 'playwright'
+    },
+    eyes_package: {
+        groupId: 'com.applitools',
+        artifactId: 'eyes-playwright-java5'
+    },
+}
+
+const appium_package = {
+    work_dir: "sdks/java/appium",
+    framework_package: {
+        groupId: 'io.appium',
+        artifactId: 'java-client'
+    },
+    eyes_package: {
+        groupId: 'com.applitools',
+        artifactId: 'eyes-appium-java5'
+    },
+}
 const base_common = base_variations.map(variant => ({...common, ...variant,}))
 const appium_common = base_common.map(variant => ({...variant, gh_environment: 'appium_latest'})).concat([
     // {...common, os:'ubuntu-latest', version:'previous@1', gh_environment: 'appium_latest'},
-    {...common, os:'ubuntu-latest', version:'latest@', gh_environment: 'appium_previous'},
+    {...common, os: 'ubuntu-latest', version: 'latest@', gh_environment: 'appium_previous'},
     // {...common, os:'ubuntu-latest', version:'previous@1', gh_environment: 'appium_previous'},
 ])
 const variations = base_common
     .map((variant) => ({
         ...variant,
+        ...selenium_package,
         use_selenium: true,
-        framework_package:{
-            groupId:'org.seleniumhq.selenium',
-            artifactId:'selenium-java'
-        },
-        eyes_package: {
-            groupId:'com.applitools',
-            artifactId:'eyes-selenium-java5'
-        },
-        work_dir: "sdks/java/selenium",
-        job_name:`Java Selenium [${variant.os} | ${variant["java-version"]} | version: ${variant.version}]`
+        job_name: `Java Selenium [${variant.os} | ${variant["java-version"]} | version: ${variant.version}]`
     }))
     .concat(base_common.map(variant => ({
         ...variant,
+        ...playwright_package,
         use_selenium: false,
-        framework_package:{
-            groupId:'com.microsoft.playwright',
-            artifactId:'playwright'
-        },
-        eyes_package: {
-            groupId:'com.applitools',
-            artifactId:'eyes-playwright-java5'
-        },
-        work_dir: "sdks/java/playwright",
-        job_name:`Java Playwright [${variant.os} | ${variant["java-version"]} | version: ${variant.version}]`
+        job_name: `Java Playwright [${variant.os} | ${variant["java-version"]} | version: ${variant.version}]`
     })))
+    .concat([
+        {
+            os: "ubuntu-latest",
+            version: "latest@",
+            ...common,
+            ...alpine,
+            ...selenium_package,
+            job_name: `Java Selenium [alpine | 8 | latest@]`
+        },
+        {
+            os: "ubuntu-latest",
+            version: "latest@",
+            ...common,
+            ...alpine,
+            ...playwright_package,
+            job_name: `Java Playwright [alpine | 8 | latest@]`
+        },
+    ])
     .concat(appium_common.map(variant => ({
         ...variant,
-        framework_package:{
-            groupId:'io.appium',
-            artifactId:'java-client'
-        },
-        eyes_package: {
-            groupId:'com.applitools',
-            artifactId:'eyes-appium-java5'
-        },
-        work_dir: "sdks/java/appium",
+        ...appium_package,
         use_appium: true,
-        job_name:`Java Appium [${variant.os} | ${variant["java-version"]} | client version: ${variant.version} | ${variant.gh_environment}] `
+        job_name: `Java Appium [${variant.os} | ${variant["java-version"]} | client version: ${variant.version} | ${variant.gh_environment}] `
     })))
 console.log(variations)
 module.exports = {
