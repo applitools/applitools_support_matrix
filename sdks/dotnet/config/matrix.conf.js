aeb7631c8834b6d976761b9805a322bdac99f271
Itai Bar-Haim <itaibh@gmail.com>
2023-06-20 14:37:30 +0300

    stabilize dotnet on support matrix (#33)
    * first attempt to stabilize all flavors of dotnet
    * fixing
    * update tests using for appium test files
    ---------
    
    Co-authored-by: Artem Borodavka <artem.borodavka@applitools.com>

--- sdks/dotnet/config/matrix.conf.js
+++ sdks/dotnet/config/matrix.conf.js
@@ -2,7 +2,7 @@
 const {getOS} = require('../../matrix/util')
 const common = {
     "dotnet-version": "7.0",
-    test_command: "dotnet test --no-build -f net7.0 -- NUnit.NumberOfTestWorkers=1",
+    test_command: "dotnet test -f net7.0 -- NUnit.NumberOfTestWorkers=1",
     "test_runner": "dotnet"
 }
 const base_variations = [
@@ -10,24 +10,24 @@ const base_variations = [
         os: "ubuntu-latest",
         version: "latest@"
     },
-    // {
-    //     os: "windows-latest",
-    //     version: "latest@"
-    // },
-    // {
-    //     os: "macos-latest",
-    //     version: "latest@"
-    // }
+    {
+        os: "windows-latest",
+        version: "latest@"
+    },
+    {
+        os: "macos-latest",
+        version: "latest@"
+    }
 ]
 const containers = [
-    // {
-    //     "dotnet-version": "7",
-    //     os: "ubuntu-latest",
-    //     version: "latest@",
-    //     use_container: true,
-    //     container_name: 'alpine',
-    //     container: 'artem0tranduil/alpine_runner:latest',
-    // },
+    {
+        "dotnet-version": "7",
+        os: "ubuntu-latest",
+        version: "latest@",
+        use_container: true,
+        container_name: 'alpine',
+        container: 'artem0tranduil/alpine_runner:latest',
+    },
     {
         "dotnet-version": "7",
         os: "ubuntu-latest",
