5f5b4f4992927d4bdb60bfaae057d73bf9363631
Artem-applitools <59561599+Artem-applitools@users.noreply.github.com>
2023-07-03 11:55:43 +0300

    Add Dotnet playwright (#35)
    * add dotnet playwright to the support matrix
    * Update nunit hooks
    * Update tests to work async
    * Add using
    * update project and playwright run command
    * update playwright install script
    * update eyes instance creation
    * check with java
    * update testname for the ufg tests on dotnet playwright

--- sdks/dotnet/config/matrix.conf.js
+++ sdks/dotnet/config/matrix.conf.js
@@ -61,6 +61,13 @@ const variations = web_common
         eyes_package: "Eyes.Selenium4",
         job_name:`Dotnet Selenium4 [${getOS(variant)} | ${variant["dotnet-version"]} | client version: ${variant.version}]`
     })))
+    .concat(web_common.map(variant => ({...variant,
+        work_dir: 'sdks/dotnet/playwright',
+        framework_package: "Microsoft.Playwright",
+        eyes_package: "Eyes.Playwright",
+        test_command: "pwsh bin/Debug/net7.0/playwright.ps1 install && dotnet test -f net7.0 -- NUnit.NumberOfTestWorkers=1",
+        job_name:`Dotnet Playwright [${getOS(variant)} | ${variant["dotnet-version"]} | client version: ${variant.version}]`
+    })))
     .concat(appium_common.map(variant => ({...variant,
         work_dir: 'sdks/dotnet/appium',
         framework_package: "Appium.WebDriver",
---
+++ sdks/dotnet/playwright/tests/config/TestBase.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Threading.Tasks;
+using Applitools;
+using Applitools.Playwright;
+using Applitools.Playwright.Fluent;
+using Applitools.Utils.Geometry;
+using Applitools.VisualGrid;
+using Microsoft.Playwright;
+using NUnit.Framework;
+using BrowserType = Applitools.BrowserType;
+namespace Applitools.Support.Matrix.Tests
+{
+        
+        public abstract class BaseTest 
+    {       
+        protected IPlaywright Playwright;
+        protected IBrowser Browser;
+        protected IBrowserContext Context;
+        protected IPage Page;
+        protected PlaywrightEyesRunner runner;
+        protected Eyes eyes;
+        
+        public static readonly BatchInfo batch = new BatchInfo("Support Matrix");
+        public static readonly bool CI = Environment.GetEnvironmentVariable("CI") == "true";
+
+        public static readonly StdoutLogHandler logger = new StdoutLogHandler();
+
+        public virtual StitchModes getStitchMode() 
+        {
+            return StitchModes.CSS;
+        }
+
+        public virtual BatchInfo getBatch() 
+        {
+            return batch;
+        }
+
+        public virtual bool isUFG() 
+        {
+            return false;
+        }
+
+        protected void initEyes()
+        {
+            runner = isUFG() ? (PlaywrightEyesRunner)(new VisualGridRunner(10, logger)) : new ClassicRunner(logger);
+            eyes = new Eyes(runner);
+            eyes.Batch = getBatch();
+            eyes.StitchMode = getStitchMode();
+            eyes.BranchName = "dotnet";
+            eyes.ParentBranchName = "master";
+            eyes.SaveNewTests = false;
+            eyes.HideScrollbars = true;
+            eyes.HideCaret = true;
+        }
+
+        public async Task browserSetup() 
+        {
+            Playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+            Browser = await Playwright.Chromium
+            .LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+            Context = await Browser.NewContextAsync();
+            Page = await Context.NewPageAsync();
+
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+        }
+      
+        [OneTimeTearDown]
+        public void OneTearDown()
+        {
+        }
+
+        [SetUp]
+        public async Task TimeSetup()
+        {   
+
+            await browserSetup();
+            initEyes();
+        }
+      
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Page.CloseAsync();
+            Playwright.Dispose();
+            eyes?.AbortIfNotClosed();
+            runner?.GetAllTestResults(false);
+        }
+
+    }
+
+
+}
