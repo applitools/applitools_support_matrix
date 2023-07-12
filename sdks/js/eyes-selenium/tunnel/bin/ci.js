d4e100f4fd3d19ddc580589df487f110fe121ed7
Artem Borodavka <artem.borodavka@applitools.com>
2023-07-12 23:24:38 +0300

    adjust command for windows

--- sdks/js/eyes-selenium/tunnel/bin/ci.js
+++ sdks/js/eyes-selenium/tunnel/bin/ci.js
@@ -2,7 +2,7 @@
 const path = require("path");
 const cp = require("child_process");
 const options = {stdio: "inherit", cwd: path.resolve(__dirname, "../")}
-const server = cp.spawn("npm", ["start"], options)
+const server = cp.spawn("node", ["./bin/www"], options)
 process.env.APPLITOOLS_EG_FRPC_CACHE_DIRECTORY = path.resolve(__dirname, "../tunnel_bin")
 setTimeout(() => {
     try {
