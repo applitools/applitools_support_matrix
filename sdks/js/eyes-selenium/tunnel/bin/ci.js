51fa5fa343a67ccf1b1b674fa26ae3994d80bb7e
Artem Borodavka <artem.borodavka@applitools.com>
2023-07-12 23:15:57 +0300

    fix spawn

--- sdks/js/eyes-selenium/tunnel/bin/ci.js
+++ sdks/js/eyes-selenium/tunnel/bin/ci.js
@@ -2,9 +2,14 @@
 const path = require("path");
 const cp = require("child_process");
 const options = {stdio: "inherit", cwd: path.resolve(__dirname, "../")}
-const server = cp.spawn("npm start", [], options)
+const server = cp.spawn("npm", ["start"], options)
+process.env.APPLITOOLS_EG_FRPC_CACHE_DIRECTORY = path.resolve(__dirname, "../tunnel_bin")
 setTimeout(() => {
-    cp.execSync("npm test", options) // Does not terminate the Node.js process in the shell.
+    try {
+        cp.execSync("npm test", options)
+    } finally {
+        server.kill();
+    }
 }, 4000);
 
 
