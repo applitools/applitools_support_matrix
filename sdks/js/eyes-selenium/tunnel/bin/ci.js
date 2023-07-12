ca07f6c1d7e25badc027c890cf1def7ef2ad2794
Artem Borodavka <artem.borodavka@applitools.com>
2023-07-12 23:01:46 +0300

    update script

--- sdks/js/eyes-selenium/tunnel/bin/ci.js
+++ sdks/js/eyes-selenium/tunnel/bin/ci.js
@@ -1,12 +1,10 @@
 "use strict";
 const path = require("path");
 const cp = require("child_process");
-const options = {stdio: "inherit", cwd: path.resolve(__dirname,"../")}
-const server = cp.spawn("npm start",[],options)
-process.env.APPLITOOLS_EG_FRPC_CACHE_DIRECTORY = path.resolve(__dirname, "../tunnel_bin")
-try {
-    cp.execSync("npm test", options )
-} finally {
-    server.kill();
-}
+const options = {stdio: "inherit", cwd: path.resolve(__dirname, "../")}
+const server = cp.spawn("npm start", [], options)
+setTimeout(() => {
+    cp.execSync("npm test", options) // Does not terminate the Node.js process in the shell.
+}, 4000);
+
 
