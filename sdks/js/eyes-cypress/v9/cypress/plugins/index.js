dc30013a7bfeb79ed34a2a7808362e444ec7537e
Artem Borodavka <artem.borodavka@applitools.com>
2023-09-22 12:13:16 +0300

    update arg for the chrome

--- sdks/js/eyes-cypress/v9/cypress/plugins/index.js
+++ sdks/js/eyes-cypress/v9/cypress/plugins/index.js
@@ -3,6 +3,19 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
+    on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome' && browser.isHeadless) {
+            launchOptions.args = launchOptions.args.map((arg) => {
+                if (arg === '--headless') {
+                    return '--headless=new'
+                }
+
+                return arg
+            })
+        }
+
+        return launchOptions
+    })
 }
 
 
