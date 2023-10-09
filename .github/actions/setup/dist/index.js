824517e97888c660a018f6763f311d7199f0b101
Artem-applitools <59561599+Artem-applitools@users.noreply.github.com>
2023-10-09 12:37:01 +0300

    Ci integration (#40)
    * update setup action
    add new workflow
    
    * update name
    
    * update checkout
    
    * update inputs
    
    * update test web options
    
    * update check workflow
    
    * add MD and update checkout ref

--- .github/actions/setup/dist/index.js
+++ .github/actions/setup/dist/index.js
@@ -2688,6 +2688,108 @@ exports["default"] = _default;
 
 /***/ }),
 
+/***/ 132:
+/***/ ((module) => {
+
+const map = new Map();
+map.set('js/eyes-webdriverio', 'sdks/js/eyes-webdriverio');
+map.set('js/eyes-testcafe', 'sdks/js/eyes-testcafe');
+map.set('js/eyes-storybook', 'sdks/js/eyes-storybook');
+map.set('js/eyes-puppeteer', 'sdks/js/eyes-puppeteer');
+map.set('js/eyes-protractor', 'sdks/js/eyes-protractor');
+map.set('js/eyes-playwright', 'sdks/js/eyes-playwright');
+map.set('js/eyes-nightwatch', 'sdks/js/eyes-nightwatch');
+map.set('js/eyes-cypress', 'sdks/js/eyes-cypress');
+map.set('js/eyes-selenium', 'sdks/js/eyes-selenium');
+map.set('java', 'sdks/java');
+map.set('dotnet', 'sdks/dotnet');
+map.set('python', 'sdks/python');
+map.set('ruby', 'sdks/ruby');
+module.exports = map
+
+/***/ }),
+
+/***/ 118:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const path = __nccwpck_require__(17)
+const fs = __nccwpck_require__(147)
+const DIR_MAP = __nccwpck_require__(132)
+
+function lastPassedFLow(sdks) {
+    let include = []
+    const filePath = path.join(process.cwd(), 'last_passed.json');
+    const json_string = fs.readFileSync(filePath).toString();
+    const last_passed = JSON.parse(json_string)
+    sdks.forEach(sdk => {
+        const matrix_jobs = last_passed.data.filter(job => job.matrix_config_dir === sdk.dir)
+        const dir_include = matrix_jobs.map(job => ({
+            ...JSON.parse(job.matrix_string),
+            version: `exact@${job.version}`,
+            eyes_version: sdk.eyes_version
+        }))
+        include = include.concat(dir_include)
+    })
+    return include;
+}
+
+function defaultFlow(sdks) {
+    let include = []
+    // Adding hardcoded string is required for ncc to build it properly into 1 file
+    const fileName = `matrix.conf.js`;
+    sdks.forEach(sdk => {
+        const filePath = path.join(process.cwd(), sdk.dir, 'config', fileName)
+        console.log(`Filepath value: ${filePath}`)
+        const readedFile = require(filePath)
+        include = include.concat(readedFile.include.map(matrix_data => ({
+            ...matrix_data,
+            matrix_config_dir: sdk.dir,
+            eyes_version: sdk.eyes_version
+        })))
+    })
+    return include;
+}
+
+function prepareSDKs(sdk_versions_json, dirs) {
+    if (sdk_versions_json) {
+        return prepareVersionsJson(sdk_versions_json)
+    }
+
+    return dirs.map(dir => ({dir: dir, eyes_version: `latest@`}))
+}
+
+function prepareInclude(sdk_versions_json, dirs, use_last_passed) {
+    let sdks = prepareSDKs(sdk_versions_json, dirs)
+    if (use_last_passed) {
+        return lastPassedFLow(sdks);
+    } else {
+        return defaultFlow(sdks)
+    }
+}
+
+function prepareVersionsJson(sdk_versions_json) {
+    const sdk_versions =  JSON.parse(sdk_versions_json)
+    if(!Array.isArray(sdk_versions)) throw new Error("Input should be an Array")
+    const checked = sdk_versions.filter(sdk => DIR_MAP.has(sdk.name))
+    if(checked.length < sdk_versions.length) {
+        sdk_versions.filter(sdk => !DIR_MAP.has(sdk.name)).forEach(sdk => {
+            console.error(`SDK with name ${sdk.name} doesn't have mapping so there support matrix tests wasn't executed for it.`)
+        })
+        throw new Error("There are was passed unsupported sdk name")
+    }
+    return checked.map(sdk => ({
+        dir: DIR_MAP.get(sdk.name),
+        eyes_version: sdk.version
+    }))
+
+}
+
+module.exports = {
+    prepareInclude
+}
+
+/***/ }),
+
 /***/ 491:
 /***/ ((module) => {
 
@@ -2818,41 +2920,22 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const {getInput, getBooleanInput, setOutput} = __nccwpck_require__(136);
-const path = __nccwpck_require__(17)
-const fs = __nccwpck_require__(147)
+const {prepareInclude} = __nccwpck_require__(118)
+
 
 const work_dir = getInput('work_dir');
+const sdk_versions_json = getInput('sdk_versions_json')
 const use_last_passed = getBooleanInput('last_passed');
 let matrix;
 
-const dirs = work_dir.split(', ')
-
-
-let include = [];
-if (use_last_passed) {
-    const filePath = path.join(process.cwd(), 'last_passed.json');
-    const json_string = fs.readFileSync(filePath).toString();
-    const last_passed = JSON.parse(json_string)
-    dirs.forEach(dir => {
-        const matrix_jobs = last_passed.data.filter(job => job.matrix_config_dir === dir)
-        const dir_include = matrix_jobs.map(job => ({
-            ...JSON.parse(job.matrix_string),
-            version: `exact@${job.version}`
-        }))
-        include = include.concat(dir_include)
-    })
-
-
-} else {
-    // Adding hardcoded string is required for ncc to build it properly into 1 file
-    const fileName = `matrix.conf.js`;
-    dirs.forEach(dir => {
-        const filePath = path.join(process.cwd(), dir, 'config', fileName)
-        console.log(`Filepath value: ${filePath}`)
-        const readedFile = require(filePath)
-        include = include.concat(readedFile.include.map(matrix_data => ({...matrix_data, matrix_config_dir: dir})))
-    })
+if(!work_dir && !sdk_versions_json) {
+    throw new Error("Missing input for required sdks to test")
 }
+
+let dirs = work_dir.split(', ')
+
+
+let include = prepareInclude(sdk_versions_json,dirs,use_last_passed);
 
 
 matrix = {include}
@@ -2879,6 +2962,7 @@ if (day % 2 === 0) {
 } else {
     setOutput("orientation", "LANDSCAPE")
 }
+
 })();
 
 module.exports = __webpack_exports__;
--- .github/actions/setup/index.js
+++ .github/actions/setup/index.js
@@ -1,39 +1,20 @@
 const {getInput, getBooleanInput, setOutput} = require("@actions/core");
-const path = require('path')
-const fs = require('fs')
+const {prepareInclude} = require("./src/util")
+
 
 const work_dir = getInput('work_dir');
+const sdk_versions_json = getInput('sdk_versions_json')
 const use_last_passed = getBooleanInput('last_passed');
 let matrix;
 
-const dirs = work_dir.split(', ')
-
-
-let include = [];
-if (use_last_passed) {
-    const filePath = path.join(process.cwd(), 'last_passed.json');
-    const json_string = fs.readFileSync(filePath).toString();
-    const last_passed = JSON.parse(json_string)
-    dirs.forEach(dir => {
-        const matrix_jobs = last_passed.data.filter(job => job.matrix_config_dir === dir)
-        const dir_include = matrix_jobs.map(job => ({
-            ...JSON.parse(job.matrix_string),
-            version: `exact@${job.version}`
-        }))
-        include = include.concat(dir_include)
-    })
-
-
-} else {
-    // Adding hardcoded string is required for ncc to build it properly into 1 file
-    const fileName = `matrix.conf.js`;
-    dirs.forEach(dir => {
-        const filePath = path.join(process.cwd(), dir, 'config', fileName)
-        console.log(`Filepath value: ${filePath}`)
-        const readedFile = require(filePath)
-        include = include.concat(readedFile.include.map(matrix_data => ({...matrix_data, matrix_config_dir: dir})))
-    })
+if(!work_dir && !sdk_versions_json) {
+    throw new Error("Missing input for required sdks to test")
 }
+
+let dirs = work_dir.split(', ')
+
+
+let include = prepareInclude(sdk_versions_json,dirs,use_last_passed);
 
 
 matrix = {include}
