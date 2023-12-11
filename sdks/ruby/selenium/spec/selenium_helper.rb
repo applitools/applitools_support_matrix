af96e1b4c08d6d59a0d70afef8cf0a8d96eac6eb
Artem-applitools <59561599+Artem-applitools@users.noreply.github.com>
2023-12-11 14:54:55 +0200

    update ruby branch

--- sdks/ruby/selenium/spec/selenium_helper.rb
+++ sdks/ruby/selenium/spec/selenium_helper.rb
@@ -29,8 +29,8 @@ RSpec.shared_context "Common" do
     eyes.configure do |conf|
       # conf.batch = $run_batch
       conf.api_key = ENV['APPLITOOLS_API_KEY']
-      conf.branch_name = 'master'
-      conf.parent_branch_name = 'master'
+      conf.branch_name = 'ruby'
+      conf.parent_branch_name = 'ruby'
       conf.save_new_tests = false
       conf.hide_caret = true
     end
